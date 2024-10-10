aa2a5a75130c9943fe6d7ac170c8edbdcbd30de0
Mayank Raj <mayankraj052@gmail.com>
2024-10-11 00:50:31 +0530

    init

--- hello-react/index.js
+++ hello-react/index.js
@@ -1,1 +1,25 @@
-console.log("This is a typescript file!!!");
+function bubbleSort(arr) {
+    var _a;
+    var n = arr.length;
+    var swapped;
+    for (var i = 0; i < n - 1; i++) {
+        swapped = false;
+        // Last i elements are already sorted
+        for (var j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                _a = [arr[j + 1], arr[j]], arr[j] = _a[0], arr[j + 1] = _a[1];
+                swapped = true;
+            }
+        }
+        // If no two elements were swapped in the inner loop, the array is sorted
+        if (!swapped) {
+            break;
+        }
+    }
+    return arr;
+}
+// Example usage:
+var numbers = [64, 34, 25, 12, 22, 11, 90];
+var sortedNumbers = bubbleSort(numbers);
+console.log('Sorted array:', sortedNumbers);
--- hello-react/index.ts
+++ hello-react/index.ts
@@ -1,1 +1,29 @@
-console.log("This is a typescript file!!!");
+function bubbleSort(arr: number[]): number[] {
+    const n = arr.length;
+    let swapped: boolean;
+
+    for (let i = 0; i < n - 1; i++) {
+        swapped = false;
+
+        // Last i elements are already sorted
+        for (let j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                swapped = true;
+            }
+        }
+
+        // If no two elements were swapped in the inner loop, the array is sorted
+        if (!swapped) {
+            break;
+        }
+    }
+
+    return arr;
+}
+
+// Example usage:
+const inputNumbers = [64, 34, 25, 12, 22, 11, 90];
+const sortedArray = bubbleSort(inputNumbers);
+console.log('Sorted array:', sortedArray);
